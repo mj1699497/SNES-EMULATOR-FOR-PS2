c897e2252dc7817b68476c70b919d23ddfcd1ba9
mj1699497 <mj1699497@gmail.com>
2025-12-17 11:09:01 -0300

    Add save and load state functions in savestate.c

---
+++ snesps2/save/savestate.c
@@ -0,0 +1,14 @@
+#include "savestate.h"
+#include "../cpu/cpu65816.h"
+#include "../ppu/ppu.h"
+#include "../apu/apu.h"
+
+void save_state()
+{
+    // salva CPU + RAM + PPU + APU + chips
+}
+
+void load_state()
+{
+    // restaura tudo
+}
