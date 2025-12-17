264c43ce0f7a63ddedec6809131add44b842696b
mj1699497 <mj1699497@gmail.com>
2025-12-17 11:10:03 -0300

    Add files via upload

---
+++ snesps2/save/savestate.h
@@ -0,0 +1,7 @@
+#ifndef SAVESTATE_H
+#define SAVESTATE_H
+
+void save_state();
+void load_state();
+
+#endif
---
+++ snesps2/save/sram.c
@@ -0,0 +1,12 @@
+#include "sram.h"
+#include "../system/memory.h"
+
+void sram_save(const char *rom)
+{
+    // grava SRAM em mass:/SNES/SAVES/
+}
+
+void sram_load(const char *rom)
+{
+    // carrega SRAM se existir
+}
