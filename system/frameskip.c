cd1bbeb5081c4dba86542331a92b7bf53091405c
mj1699497 <mj1699497@gmail.com>
2025-12-17 11:14:24 -0300

    Delete system directory

--- system/frameskip.c
+++
@@ -1,4 +0,0 @@
-void frameskip_update(int frame_time)
-{
-    skip_next = (frame_time > 16600);
-}
--- system/timing.c
+++
@@ -1,5 +0,0 @@
-void frame_wait()
-{
-    while (get_ticks() < next_frame_tick);
-    next_frame_tick += FRAME_TICKS;
-}
